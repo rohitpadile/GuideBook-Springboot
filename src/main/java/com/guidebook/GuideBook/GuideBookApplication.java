1b2019788a966f3765e3f1eb706eb32443a38d3e
Rohit Padile <rohitpadile24@gmail.com>
2024-06-26 18:56:49 +0530

    initial commit

---
+++ src/main/java/com/guidebook/GuideBook/GuideBookApplication.java
@@ -0,0 +1,13 @@
+package com.guidebook.GuideBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guidebook/GuideBook/GuideBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidebook.GuideBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
