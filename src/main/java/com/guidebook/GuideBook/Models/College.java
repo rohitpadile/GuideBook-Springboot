36e5c9dd80ffb7644e18b00fbc43de582726344a
Rohit Padile <rohitpadile24@gmail.com>
2024-07-03 21:22:00 +0530

    Organised Student Profile property ideas - commented out in the entity class

--- src/main/java/com/guidebook/GuideBook/Models/College.java
+++ src/main/java/com/guidebook/GuideBook/Models/College.java
@@ -25,7 +25,6 @@ public class College {
     private Long collegeId;
     private String collegeName;
 
-     //Keep convention: Add branch to collegebranchList when a new branch is added
     @ManyToMany //owning side
     @JoinTable(name = "college_branches",
         joinColumns = @JoinColumn(name = "collegeId"),
@@ -37,5 +36,7 @@ public class College {
     @OneToMany(mappedBy = "studentCollege")
     private List<Student> collegeStudentList;
 
+    //Add Entrance Exam property here to filter colleges based on selected Entrance-Exam.
+    //THIS CAN BE ADDED IN FUTURE ALSO
 }
 
--- src/main/java/com/guidebook/GuideBook/Models/StudentProfile.java
+++ src/main/java/com/guidebook/GuideBook/Models/StudentProfile.java
@@ -21,26 +21,46 @@ public class StudentProfile {
     Long studentProfileId;
 
     Long studentMis;
-}
+    String studentCityOfCoaching;
+    String studentAboutSection; //List<String> is needed.
+    //Displaying city of coaching will help students/parents to instantly get help from students
+    //living in same city as that
+    //Do not display name of coaching institute - that may voilate laws maybe. Just research on this.
+    String studentScoreDetails;
+    String studentOtherExamScoreDetails;
 
+    String studentAcademicActivity; //activity included achievements too
+    String studentCoCurricularActivity;
+    String studentExtraCurricularAchievements;
+    String studentTutoringExperience;
+    Long studentSessionsConducted; //Sessions conducted on our platform
+
+}
+//**studentAboutSection:-
+    //IMPORTANT: "I Offer customized study plans and stress management techniques",
+    // written like websites shows comments of people that have used their websites
+    // like freecodecamp does on their websites
+    //more "studyPlan": "I offer Detailed weekly study plan focusing on concept clarity and practice"
+    //So a comment should be saying what exactly the student can tell you clearly about in the session
+    //example: "I offer best Textbook suggestions, coaching suggestions"
+
+//**studentScoreDetails - INCLUDE WHATEVER SCORE THE STUDENT WANT TO INCLUDE
 //        "jeeRank": 1500,
 //        "jeeScore": 320,
-//        "coachingInstitute": "Resonance",
-//        "mockTestScores": {
-//        "Physics": 110,
-//        "Chemistry": 105,
-//        "Mathematics": 105
-//        },
-//        "studyPlan": "Detailed weekly study plan focusing on concept clarity and practice",
+//        "Physics": 110, say - "I scored highest in physics./ I love physics"
+//        "Chemistry": 105, - "I scored highest in chemistry./ I love chemistry"
+//        "Mathematics": 105 - "I scored highest in mathematics./ I love mathematics"
 
+//**studentOtherExamScoreDetails
 //        "otherExamsQualified": ["BITSAT - 350", "VITEEE - Rank 200"],
-//        "researchProjects": ["Project on Renewable Energy", "Paper on Electric Vehicles"],
-//        "internships": ["Intern at Siemens", "Workshop on Robotics"],
+
+//**studentAcademicActivity, studentCoCurricularActivity, studentExtraCurricularAchievements,
 //        "leadershipRoles": ["President of Robotics Club", "Organized National Level Tech Fest"],
 //        "competitions": ["Won 1st Prize in National Science Olympiad", "2nd Place in Coding Competition"],
-//        "tutoringExperience": "2 years of tutoring JEE aspirants",
 //        "workshopsConducted": ["How to Crack JEE", "Stress Management during Exams"],
 
-//IMPORTANT: "I Offer customized study plans and stress management techniques", written like websites shows comments of people that have used their websites like freecodecamp
-//Give a personalize experience on each page - context in each page are differnt
+//**studentTutoringExperience
+//        "tutoringExperience": "2 years of tutoring JEE aspirants",
 
+//**Refer LinkedIn Profile for more
+//        "internships": ["Intern at Siemens", "Workshop on Robotics"], //REFER LINKEDIN
