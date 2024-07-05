db4904c999dd34c336b04ec5156eec486e73ffff
Rohit Padile <rohitpadile24@gmail.com>
2024-07-05 18:20:32 +0530

    undone some changes leading to fatal errors

---
+++ src/main/java/com/guidebook/GuideBook/dtos/GetCollegeListForExamResponse.java
@@ -0,0 +1,14 @@
+package com.guidebook.GuideBook.dtos;
+
+import jakarta.validation.Valid;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Valid
+@Data
+public class GetCollegeListForExamResponse {
+
+    private List<String> collegeNameList = new ArrayList<>();
+}
