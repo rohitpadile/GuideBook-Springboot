da379aa8ad56fdaddb9b12036e6fb8b5a52bac0c
Rohit Padile <rohitpadile24@gmail.com>
2024-07-06 11:04:03 +0530

    Created Entity Client. Maybe its id should be BigInteger for lot of traffoc

--- src/main/java/com/guidebook/GuideBook/Models/Client.java
+++ src/main/java/com/guidebook/GuideBook/Models/Client.java
@@ -1,42 +1,46 @@
-//package com.guidebook.GuideBook.Models;
-//
-//import jakarta.persistence.*;
-//import jakarta.validation.constraints.Pattern;
-//import lombok.*;
-//import lombok.experimental.FieldDefaults;
-//import org.hibernate.annotations.CreationTimestamp;
-//import org.hibernate.annotations.UpdateTimestamp;
-//
-//import java.util.Date;
-//
-//@Entity
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Data //getters and setters
-//@Builder
-//@FieldDefaults(level = AccessLevel.PRIVATE)
-//public class Client {
-//    String clientFirstName;
-//    String clientMiddleName;
-//    String clientLastName;
-//    String clientEmail;
+package com.guidebook.GuideBook.Models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data //getters and setters
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long clientId;
+    String clientFirstName;
+    String clientMiddleName;
+    String clientLastName;
+    @Column(unique = true)
+    String clientEmail;
 //    @Pattern(regexp = "^\\+?[0-9. ()-]{6,20}$", message = "Invalid phone number")
-//    //PATTERN MATCHES FOR GENERAL NUMBER FOR ANY COUNTRY.
-//    String clientPhoneNumber;
-//    Integer clientAge;
-//    String clientCollege;
-//    @Lob //for storing large data
-//    byte[] clientProofDoc; //college id, college fees, government id
-//    //VERY IMPORTANT NOTE:-
-//    //REJECT THE SESSION IF THE PROOF IS NOT VALID.
-//    //IF THE STUDENT CONFIRMS THE SESSION WITH A NON VALID PROOF, FIRE HIM/HER
-//
-//    @CreationTimestamp
-//    @Temporal(TemporalType.TIMESTAMP)
-//    Date createdOn;
-//    @UpdateTimestamp
-//    @Temporal(TemporalType.TIMESTAMP)
-//    Date updatedOn;
-//    @Version
-//    private Integer version;
-//}
+    //PATTERN MATCHES FOR GENERAL NUMBER FOR ANY COUNTRY.
+    String clientPhoneNumber;
+    Integer clientAge;
+    String clientCollegeName;
+    @Lob //for storing large data
+    byte[] clientProofDoc; //college id, college fees, government id
+    //VERY IMPORTANT NOTE:-
+    //REJECT THE SESSION IF THE PROOF IS NOT VALID.
+    //IF THE STUDENT CONFIRMS THE SESSION WITH A NON VALID PROOF, FIRE HIM/HER
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    Date createdOn;
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    Date updatedOn;
+    @Version
+    private Integer version;
+}
