d1f683ddd6dcb7a21944f38d69ee55cd785017e5
Rohit Padile <rohitpadile24@gmail.com>
2024-07-08 00:46:08 +0530

    added account page with a utility class - deciding the currentloggedInProfile

---
+++ src/main/java/com/guidebook/GuideBook/account/UtilityClass.java
@@ -0,0 +1,11 @@
+package com.guidebook.GuideBook.account;
+
+import lombok.Data;
+
+@Data
+public class UtilityClass {
+
+    public static String currentLoggedInProfile;
+    //this will decide what the profile page to the logged in user look like
+
+}
