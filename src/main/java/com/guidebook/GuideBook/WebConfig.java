55854780659a386fb5081e69dbf51b2180c28419
Rohit Padile <rohitpadile24@gmail.com>
2024-07-16 23:22:51 +0530

    Website is working on AWS Successfully

--- src/main/java/com/guidebook/GuideBook/WebConfig.java
+++ src/main/java/com/guidebook/GuideBook/WebConfig.java
@@ -8,17 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/**")
-                        .allowedOrigins("http://guidebookx.s3-website.ap-south-1.amazonaws.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/admin/**")
+                .allowedOrigins("http://guidebookx.s3-website.ap-south-1.amazonaws.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
