23918ad4186f41ba6a459accee86771772915501
Rohit Padile <rohitpadile24@gmail.com>
2024-08-02 00:55:34 +0530

    Start working for filtered branches for college endpoint and changes in react

--- src/main/java/com/guidebook/GuideBook/TR/Controller/TRBranchController.java
+++ src/main/java/com/guidebook/GuideBook/TR/Controller/TRBranchController.java
@@ -1,14 +1,16 @@
 package com.guidebook.GuideBook.TR.Controller;
 
+import com.guidebook.GuideBook.ADMIN.Controller.BranchController;
+import com.guidebook.GuideBook.ADMIN.dtos.selectStudentFiltering.GetAllBranchNameListForCollegeRequest;
+import com.guidebook.GuideBook.ADMIN.dtos.selectStudentFiltering.GetAllBranchNameListForCollegeResponse;
+import com.guidebook.GuideBook.ADMIN.exceptions.BranchNotFoundException;
 import com.guidebook.GuideBook.TR.Services.TRBranchService;
 import com.guidebook.GuideBook.TR.dtos.TRGetAllBranchNameResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = {
         "http://localhost:3000", "http://localhost:8080",
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/teamRecruiter/")
 public class TRBranchController {
     private TRBranchService trBranchService;
+    private BranchController branchController;
     @Autowired
     public TRBranchController(TRBranchService trBranchService) {
         this.trBranchService = trBranchService;
@@ -28,5 +31,12 @@ public class TRBranchController {
     public ResponseEntity<TRGetAllBranchNameResponse> getAllBranchNames(){
         TRGetAllBranchNameResponse res = trBranchService.getAllBranchNames();
         return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+    @PostMapping("/filteredBranchesForCollege")
+    public ResponseEntity<GetAllBranchNameListForCollegeResponse> getAllBranchNameListForCollege(
+            @RequestBody @Valid GetAllBranchNameListForCollegeRequest request)
+            throws BranchNotFoundException
+    {
+        return branchController.getAllBranchNameListForCollege(request);
     }
 }
